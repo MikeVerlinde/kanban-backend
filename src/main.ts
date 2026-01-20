c67302977a544cce5eda61fd776b7ae7abba9a33
Mike <mike.verlinde@outlook.com>
2026-01-20 12:24:45 +0100

    feat: add cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   app.use(cookieParser(process.env.COOKIE_SECRET))
   await app.listen(process.env.PORT ?? 3000);
 }
